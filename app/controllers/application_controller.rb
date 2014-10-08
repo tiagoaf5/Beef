cf247bfc38922a8bf15e8a76ac51515a8ec9346a
Tiago Fernandes <ei11054@fe.up.pt>
2014-10-08 09:29:52 +0100

    Added login to main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   "..def show
     render action: 'welcome/index'
   end"
+
+
 
 end
