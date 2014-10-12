1a465a8de60b04ae62f2240b3b1c5efc3851f542
vagrant <vagrant@rails-dev-box>
2014-10-12 17:48:44 +0000

    Skeleton for bets history functionality

---
+++ app/assets/javascripts/leagues.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".btn").popover({
+    html: "true",
+    content:"<table class=\"table\"><tr><td>Chico</td><td>Porto FC</td><td>5:0</td><td>Benfica FC</td></tr><tr><td>Moska</td><td>Porto FC</td><td>10:0</td><td>Benfica FC</td></tr></table>"
+    });
+});
