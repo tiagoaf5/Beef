3030eef9662033c39d6ec879985d4332506ba968
dvn123 <diogovaznunes@gmail.com>
2014-10-16 20:48:42 +0100

    More model validations

--- app/models/league.rb
+++ app/models/league.rb
@@ -8,4 +8,6 @@ class League < ActiveRecord::Base
   has_many :championships, through: :league_championships
   has_many :users, through: :league_users
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
+
+  validates :score_correct, :score_difference, :score_prediction,  numericality: { :greater_than_or_equal_to => 0}
 end
