b5e7a6494111ec6f0db26931e681c80bb751c169
Tiago Fernandes <ei11054@fe.up.pt>
2014-10-17 20:15:48 +0100

    replaced text with icons on games

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    if user_signed_in?
-      redirect_to(leagues_path)
-    end
+     if user_signed_in?
+       redirect_to(leagues_path)
+     end
   end
 
 end
