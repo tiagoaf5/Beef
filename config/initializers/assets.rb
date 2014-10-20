6cf638727565c475bc0dc2c6c1ae9a9808a303b2
MiguelGPereira <miguelgopereira@gmail.com>
2014-10-20 19:09:22 +0100

    Assets configuration for production environment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/*"
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/stylesheets"
+Rails.application.config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(eot|svg|ttf|woff)\z/
+    true
+  end }
