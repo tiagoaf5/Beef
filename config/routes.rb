4e1444d74412d99b76789fa5ac7e855b5ae8f123
MiguelGPereira <miguelgopereira@gmail.com>
2014-11-10 20:38:50 +0000

    Logout fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  devise_scope :user do
+    get '/users/sign_out', :to => 'devise/sessions#destroy', :as => :sign_out
+  end
 
  # devise_scope :user do
  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
