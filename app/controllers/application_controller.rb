52283d178d35e1c86190bb0fe8ce89078fc3874d
MiguelGPereira <miguelgopereira@gmail.com>
2014-11-10 21:34:17 +0000

    Add parameter :name to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,19 @@ class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
   before_action :get_leagues
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+
   def index
 
   end
+
 
   private
 
