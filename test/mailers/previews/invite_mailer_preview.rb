3f768bb7b405ade168449dc078501fc08a958453
dvn123 <diogvaznunes@gmail.com>
2014-11-26 00:20:52 +0000

    Email finished

--- test/mailers/previews/invite_mailer_preview.rb
+++ test/mailers/previews/invite_mailer_preview.rb
@@ -1,3 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/invite_mailer
 class InviteMailerPreview < ActionMailer::Preview
   def invite_email
     InviteMailer.invite_email(User.first, 'diogovaznunes@gmail.com', 'asd')
